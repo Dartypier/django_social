516d4ccc457bf29253b37823e338abe869c11002
jacopo <jacopo.zecchi@gmail.com>
2023-05-27 17:22:25 +0200

    image auto resize imrpoved

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from PIL import Image
 
 
 class CustomUser(AbstractUser):
+    # presets (AbstractUser):
+    # username
+    # date_joined
     first_name = models.CharField(max_length=150, blank=False, null=False)
     last_name = models.CharField(max_length=150, blank=False, null=False)
     bio = models.TextField(max_length=200, blank=True, null=True)
@@ -13,32 +16,40 @@ class CustomUser(AbstractUser):
         upload_to="avatar", default="user_avatar.png", blank=False
     )
 
-    # Image avatar resize by Pillow
+    # Image avatar crop by Pillow
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
         if self.avatar:
-            # Open the image using Pillow
             image = Image.open(self.avatar.path)
 
-            # Resize the image to the desired dimensions
-            desired_width = 300
-            desired_height = 300
-            resized_image = image.resize(
-                (desired_width, desired_height), Image.ANTIALIAS
-            )
+            # Calculate the aspect ratio of the image
+            aspect_ratio = image.width / image.height
 
-            # Save the resized image back to the avatar field
-            resized_image.save(self.avatar.path)
+            # Determine the dimensions for cropping
+            if aspect_ratio > 1:
+                # Landscape orientation
+                new_width = image.height
+                new_height = image.height
+            else:
+                # Portrait or square orientation
+                new_width = image.width
+                new_height = image.width
 
-    # presets:
-    # username
-    # date_joined
+            # Calculate the coordinates for cropping
+            left = (image.width - new_width) // 2
+            top = (image.height - new_height) // 2
+            right = left + new_width
+            bottom = top + new_height
 
-    ##Slug for using username instead of pk in urls
+            # Crop the image
+            cropped_image = image.crop((left, top, right, bottom))
 
-    # def __str__(self):
-    #     return str(self.pk)
+            # Resize the cropped image to the desired size
+            cropped_image = cropped_image.resize((600, 600), Image.ANTIALIAS)
+
+            # Save the cropped and resized image back to the avatar field
+            cropped_image.save(self.avatar.path)
 
 
 # follow/unfollow model
